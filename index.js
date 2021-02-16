4dcfacc723dbb16dad4284da641e8bc9771f4ef4
Carlos Reyes <carmandoreyes93@gmail.com>
2021-02-15 18:50:41 -0600

    asd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log();
 });
 
